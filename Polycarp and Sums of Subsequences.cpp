f36c89e988dcc63399b6bd4f966c2a165957f85e
ahmed samir sakr <89707420+ahmedsamir010@users.noreply.github.com>
2022-01-19 10:44:49 +0200

    Add files via upload

---
+++ Polycarp and Sums of Subsequences.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include<iomanip>
+#include<cmath>
+#include<string>
+#include<algorithm>
+#define ahmed  ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL);
+using namespace std;
+#define ll long long
+#define ld long double
+int counter[1000001] = {};
+int main()
+{
+    ahmed;
+    ll t = 0;ll arr[10000];
+    cin >> t;
+    while (t--) {
+        for (int i = 0; i < 7; i++) {
+            cin >> arr[i];
+        }
+        ll mx = *max_element(arr, arr + 7);
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                for (int k = 0; k < 7; k++) {
+                    if (arr[i] + arr[j + 1] + arr[k + 2] == mx) {
+                        cout << arr[i] << ' ' << arr[j + 1] << ' ' << arr[k + 2] << endl; break;
+                    }
+                }
+                break;
+            }
+            break;
+        }
+    }
+    return 0;
+}
+//مهما الدنيا إتنيلت ضلمت لازم يبقى عندك زفت أمل و تعرف إن في نور في أخر أم النفق//
+
+        //اهو بنتنيل نستمر ف السعي رغم فقدان الزفت الشغف 
